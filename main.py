18760c8e7b85bcb42f6e700ead4fcd1af7d69069
Mikhail <mpanov@tetrika.school>
2022-07-10 17:43:02 +0300

    Got first results of a valid output document

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 
 def read_structure(filename='base.tex'):
@@ -9,21 +10,48 @@ def read_structure(filename='base.tex'):
 
 
 def main(*args):
-    print(f'Number of arguments: {len(sys.argv)}')
-    for argument in sys.argv:
-        print(argument)
+    final_file = []
+    # print(f'Number of arguments: {len(sys.argv)}')
+    # for argument in sys.argv:
+    #     print(argument)
     in_filename = sys.argv[1]
     out_filename = sys.argv[2]
-    n_vars = sys.argv[3]
+    n_vars = int(sys.argv[3])
+
+    # read doc structure
     with open(in_filename) as base_file:
         base_structure = base_file.read()
     file_start, base_structure = base_structure.split('%%doc_start')
     base_structure, file_end = base_structure.split('%%doc_end')
-    tasks = base_structure.split('%%task_start')
+    variant_start = base_structure.split('%%task_start')[0]
+    tasks = base_structure.split('%%task_start')[1:]
+    task_variants = {}
 
+    # read task variants
+    for i, _ in enumerate(tasks):
+        variants_filename = f'files/Problem{i+1}.tex'
+        with open(variants_filename) as input_file:
+            task_variants[i] = input_file.read().split('%%new_st')
+            for j, _ in enumerate(task_variants[i]):
+                task_variants[i][j] = task_variants[i][j].split('\n')
+                task_variants[i][j] = list(filter(None, task_variants[i][j]))
 
+    # write variants to file
+    final_file = file_start
+    for i in range(n_vars):
+        #create variant
+        variant = variant_start
+        variant = variant.replace('%%var_ph', str(i+1))
+        for i, _ in enumerate(tasks):
+            variant += tasks[i]
+            for j, _ in enumerate(task_variants[i]):
+                variant = variant.replace('%%subtask_ph', random.choice(task_variants[i][j]), 1)
+        final_file += variant
+    final_file += file_end
+
+    with open(out_filename, 'w') as output_file:
+        output_file.write(final_file)
 
 
 if __name__ == '__main__':
     main()
-
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@ Base structure file
 %%doc_start - starting part of the document. Will show up only one time for a file independent on number of variants.
 %%doc_end - finishing part of the document. Will show up once per document.
 %%var_ph - (variant_placeholder) placeholder for variant number.
-%%task_start - delimiter for different tasks. First occurrence matches first task, second matches second and so on.
+%%task_del - delimiter for different tasks. First occurrence matches first task, second matches second and so on.
 %%subtask_ph - placeholder for subtasks in the task. N-th occurrence corresponds to the n-th subtask.
 
 Problem file
