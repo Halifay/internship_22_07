b9527dc8e8078dfd0396efeb58a8b8bf42c08c77
Mikhail <mpanov@tetrika.school>
2022-08-02 16:41:32 +0300

    Added support for additional words for subtask. Placeholder word_ph and delimiter items_del.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def read_structure(filename='base.tex'):
     return base_structure
 
 
-def main(in_filename = 'files/base.tex', out_filename='files/output.tex', n_vars=3, *args):
+def main(in_filename = 'files/base.tex', out_filename='files/output.tex', n_vars=4, *args):
     final_file = []
     print(f'Number of arguments: {len(sys.argv)}')
     for argument in sys.argv:
@@ -22,18 +22,28 @@ def main(in_filename = 'files/base.tex', out_filename='files/output.tex', n_vars
     base_structure, file_end = base_structure.split('%%doc_end')
     variant_start = base_structure.split('%%task_start')[0]
     tasks = base_structure.split('%%task_start')[1:]
-    task_variants = {}
+    # creating lists with length of number of tasks
+    task_variants = [[] for i in range(len(tasks))]
+    word_lists = [[] for i in range(len(tasks))]
 
     # read task variants
-    for i, _ in enumerate(tasks):
+    for i in range(len(tasks)):
         variants_filename = f'files/Problem{i+1}.tex'
         with open(variants_filename) as input_file:
             task_variants[i] = input_file.read().split('%%new_st')
-            for j, _ in enumerate(task_variants[i]):
+            word_lists[i] = [[] for x in task_variants[i]]
+            for j in range(len(task_variants[i])):
                 task_variants[i][j] = task_variants[i][j].split('%%st_del')
                 task_variants[i][j] = list(filter(None, task_variants[i][j]))
+                # print([[] for x in task_variants[i][j]])
+                word_lists[i][j] = [[] for x in task_variants[i][j]]
+                for k, variant in enumerate(task_variants[i][j]):
+                    if len(variant.split('%%items_del')) == 2:
+                        task_variants[i][j][k] = variant.split('%%items_del')[0]
+                        word_lists[i][j][k] = variant.split('%%items_del')[1][2:-2].split(',')
+                        # print(variant.split('%%items_del')[1], len(variant.split('%%items_del')[1]))
 
-    # write variants to file
+    # write variants to the file
     final_file = file_start
     tasks_numbers = [[[] for j in range(len(task_variants[i]))] for i in range(len(task_variants))]
     for i in range(n_vars):
@@ -48,6 +58,9 @@ def main(in_filename = 'files/base.tex', out_filename='files/output.tex', n_vars
                 variant_choice_number = random.choice(tasks_numbers[i][j])
                 tasks_numbers[i][j].remove(variant_choice_number)
                 variant = variant.replace('%%subtask_ph', task_variants[i][j][variant_choice_number], 1)
+                for word in word_lists[i][j][variant_choice_number]:
+                    # print(word, len(word))
+                    variant = variant.replace('%%word_ph', word, 1)
         final_file += variant
     final_file += file_end
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,10 @@ Base structure file
 %%var_ph - (variant_placeholder) placeholder for variant number.
 %%task_del - delimiter for different tasks. First occurrence matches first task, second matches second and so on.
 %%subtask_ph - placeholder for subtasks in the task. N-th occurrence corresponds to the n-th subtask.
+%%word_ph - placeholder for varying words
 
 Problem file
-%%new_st - delimiter between batches of subtasks. (outdated) Inside one batch subtask variants are divided by new line (\n).
+%%new_st - delimiter between batches of subtasks. Inside one batch subtask variants are divided by %%st_del.
 %%st_del - delimiter betweeen subtasks inside one batch.
+%%items_del - delimiter for list of words that depend on subtask. Words should be written on the next
+    line, end with new line and use following format: [first,second,...,last]
